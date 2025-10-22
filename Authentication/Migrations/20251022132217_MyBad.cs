d14b60153a99c0a26267dcfe0014612770e17f3a
Zoran Horvat <zh@codinghelmet.com>
2025-10-22 23:21:18 +0100

    Done.

---
+++ Authentication/Migrations/20251022132217_MyBad.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Authentication.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyBad : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "auth");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserTokens",
+                newName: "AspNetUserTokens",
+                newSchema: "auth");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                newName: "AspNetUsers",
+                newSchema: "auth");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserRoles",
+                newName: "AspNetUserRoles",
+                newSchema: "auth");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserLogins",
+                newName: "AspNetUserLogins",
+                newSchema: "auth");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserClaims",
+                newName: "AspNetUserClaims",
+                newSchema: "auth");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoles",
+                newName: "AspNetRoles",
+                newSchema: "auth");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoleClaims",
+                newName: "AspNetRoleClaims",
+                newSchema: "auth");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "AspNetUserTokens",
+                schema: "auth",
+                newName: "AspNetUserTokens");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUsers",
+                schema: "auth",
+                newName: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserRoles",
+                schema: "auth",
+                newName: "AspNetUserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserLogins",
+                schema: "auth",
+                newName: "AspNetUserLogins");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetUserClaims",
+                schema: "auth",
+                newName: "AspNetUserClaims");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoles",
+                schema: "auth",
+                newName: "AspNetRoles");
+
+            migrationBuilder.RenameTable(
+                name: "AspNetRoleClaims",
+                schema: "auth",
+                newName: "AspNetRoleClaims");
+        }
+    }
+}
